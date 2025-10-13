88fea6f1617b57055829febbb4f1c9980f648b08
apple97 <rkt1313@naver.com>
2025-10-13 09:48:48 +0900

    router base도 수정

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/hobby2">
       <div className="app">
         <Routes>
           <Route path="/" element={<HomePage />} />
